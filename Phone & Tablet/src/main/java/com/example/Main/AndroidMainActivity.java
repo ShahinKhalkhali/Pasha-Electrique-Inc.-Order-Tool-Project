6b115450e42cde7f19c6b6152f22e499398da79d
Shahin Khalkhali <shahin.khalkhali94@hotmail.com>
2023-10-29 17:43:46 -0400

    Added layouts and activities for individual imageview's

--- Phone & Tablet/src/main/java/com/example/Main/AndroidMainActivity.java
+++ Phone & Tablet/src/main/java/com/example/Main/AndroidMainActivity.java
@@ -1,5 +1,6 @@
 package com.example.Main;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +11,21 @@ public class AndroidMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        // Orient main layout to landscape
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        setContentView(R.layout.layout);
+
+        // Button bindings and onClick listeners
+        findViewById(R.id.imageTECK).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, TECKActivity.class)));
+        findViewById(R.id.imageLUMEX).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, LUMEXActivity.class)));
+        findViewById(R.id.imageBX).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, BXActivity.class)));
+        findViewById(R.id.imageBARE).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, BAREActivity.class)));
+        findViewById(R.id.image18BROWN).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, BROWN18Activity.class)));
+        findViewById(R.id.imageNMWU).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, NMWUActivity.class)));
+        findViewById(R.id.imageCAT).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, CATActivity.class)));
+        findViewById(R.id.imageSOLSTR).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, SOLSTRActivity.class)));
+        findViewById(R.id.imageTOOLS).setOnClickListener(v -> startActivity(new Intent(AndroidMainActivity.this, TOOLActivity.class)));
+
     }
 }
---
+++ Phone & Tablet/src/main/java/com/example/Main/BAREActivity.java
@@ -0,0 +1,15 @@
+package com.example.Main;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ordertool.R;
+
+public class BAREActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
